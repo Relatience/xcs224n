92b3c9efbe51a88b0a5f271fe19a10463067d336
Relatience <david.dekker@relatience.com>
2021-08-18 15:34:27 +0200

    nd adjust

--- XCS224N-A5/src/submission/run.py
+++ XCS224N-A5/src/submission/run.py
@@ -137,7 +137,7 @@ elif args.function == 'finetune':
     ###         num_workers=4
 
     ### START CODE HERE
-    nd = dataset.NameDataset(args.finetune_corpus_path) #
+    nd = dataset.NameDataset(args.finetune_corpus_path, pretrain_dataset) #
     if args.reading_params_path is None:
         tconf = trainer.TrainerConfig(max_epochs=75, batch_size=256, learning_rate=6e-4,
                         lr_decay=True, warmup_tokens=512*20, final_tokens=200*len(pretrain_dataset)*block_size,
