519b86b16fa2c13e71c2908297424cfd798af05f
Relatience <david.dekker@relatience.com>
2021-08-30 10:10:07 +0200

    SynAtt_1

--- XCS224N-A5/src/submission/attention.py
+++ XCS224N-A5/src/submission/attention.py
@@ -96,6 +96,46 @@ class SynthesizerAttention(nn.Module):
         ###       How do these map to the matrices in the handout?
 
         ### START CODE HERE
+        #def forward(self, x, layer_past=None):
+            # (B x T x C) is of dimension (batch x block_size x n_embd) which is (batch x l x d) in the handout.
+            # nh should be number_of_heads, and hs would then stand for n_embed (or "dimensionality" d in the handout) per head (so that is d/h)
+
+        B, T, C = x.size()
+
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        k = self.key(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = self.query(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = self.value(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        ##
+        # Hi all, I am a little confused by the "init code" in SynthesizerAttention class. There, self.w2 and self.b2 are defined. But I don't know 
+        # why the shape has one element with config.block_size-1? Shouldn't we compute a matrix with shape block_size x block_size?
+        #
+        # Barthold (CF): Interesting question! I believe this is due to the key contribution of the synthesizer, ie replacing the dot product in 
+        # self-attention. As you can see in the handout and in the code, for CausalSelfAttention we can instantiate all linear layers with the hyper parameter d 
+        # (config.n_embd) due to the nice dot products making sequence lengths, l, "matching". However, for the synthesizer we can do this only for 
+        # the first "layer" (A in in the handout, w1 in the code). The second "layer" (B or w2) is of dimension d/h x l and, hence, dependent on the 
+        # sequence length which may vary. And this is, where I think nn.Parameters() comes in handy. It is more or less the same as nn.Linear() but 
+        # you can slice it. This allows you (hint for implementation) to slice w2 and b2, which have been initialized with config.block_size-1 
+        # (maximum possible length) to an fitting size depending on actual input length.
+        # BTW the authors stress your point explicitly in their paper: "On Parameters Depending on Sequence Length Random and dense Synthesizers both 
+        # rely on parameters that depend on length l. In general, we define a maximum length and dynamically truncate to the actual length of each 
+        # batch. We note that this is in similar spirit to trainable positional encodings which have been common practice in Transformer models. Hence, 
+        # we do not forsee any issue here. In the case that this is really a problem, one potential solution is to project to a smaller value b and 
+        # tile b to the maximum sequence length. We leave this exploration to future work." HTH (and is a correct interpretation)!
+        ##
+        # self.w1 (is A in the hand-out), self.w2 (is B in the hand-out, is of dimension d/h x l) and self.b2
+        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
+        #att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = torch.nn.ReLU(self.w1) @ self.w2 + self.b2
+        att = att.masked_fill(self.mask[:,:,:T,:T] == 0, -1e10) # todo: just use float('-inf') instead?
+        att = F.softmax(att, dim=-1)
+        att = self.attn_drop(att)
+        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+
+        # output projection
+        y = self.resid_drop(self.proj(y))
+        return y    
         ### END CODE HERE
 
         raise NotImplementedError
